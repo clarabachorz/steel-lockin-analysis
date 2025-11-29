e6818a8ca650dcac203892749f4797c0cc43b083
clarabachorz <clara.bachorz@pik-potsdam.de>
2025-11-29 09:48:46 +0100

    add energy/non-energy breakdown to AAC plots

--- analysis/R/abatement_costs.R
+++ analysis/R/abatement_costs.R
@@ -24,12 +24,16 @@ library(ggpattern)
 #' @return clean data frame
 prepare_lcop_df <- function(df) {
     df %>%
-        filter(!component == "Emission Cost") %>%
+        filter(component != "Emission Cost") %>%
         filter(between(period, 2025, 2075)) %>%
-        select(-color)
+        select(-color) %>%
+        mutate(category = ifelse(str_detect(component, regex("Electricity|H2|Hydrogen|Gas|Liquids|Solids", ignore_case = TRUE)),
+            "energy",
+            "non-energy"))
         # %>%
         # mutate(value = ifelse(route == "DRI-EAF H2", ifelse(component == "Hydrogen", 0, value), value)) %>%
         # mutate(value = ifelse(component == "Electricity", 0, value))
+    
 }
 
 #' Calculate total (average) LCOP by region and period
@@ -40,7 +44,7 @@ prepare_lcop_df <- function(df) {
 #' @return summarized data frame
 summarise_lcop_total <- function(df) {
     df %>%
-        group_by(period, region) %>%
+        group_by(period, region, category) %>%
         summarise(value = sum(value, na.rm = TRUE)) %>%
         ungroup()
 }
@@ -191,7 +195,8 @@ calc_demand_adjustment <- function(scenario1_mif, scenario2_mif, scenario2_prod,
 add_demand_adjustment <- function(scenario2_costs, demand_value) {
     scenario2_costs %>%
         left_join(demand_value, by = c("period", "region")) %>%
-        mutate(total_cost = total_cost + demand_value) %>%
+        # we add the demand adjustment to the "non-energy" category
+        mutate(total_cost = ifelse(category == "non-energy", total_cost + demand_value, total_cost)) %>%
         select(-demand_value)
 }
 
@@ -250,6 +255,50 @@ calc_cumulated_discounted_sum <- function(lockin_mif_em, start_date, end_date, d
         ungroup()
 }
 
+#' Calculate cumulated discounted sum of the cost column: as this is split in energy and non-energy categories
+#' @param lockin_mif_em Data frame with costs or emissions
+#' @param start_date Start year (e.g., 2025) for the discounting, and for the cumulative sum if it is taken
+#' @param end_date End year (e.g., 2075) for the discounting, and for the cumulative sum if it is taken
+#' @param discount_rate Discount rate (e.g., 0.05 for 5%)
+#' @param col_to_sum Column name to sum over (e.g., "total_cost" or "total_CO2_yearly")
+#' @param final_col_name Name of the final column with the cumulated discounted sum (e.g., "cost1" or "em1")
+#' @param by_decade Logical, whether to return the cumulated sum by decade (TRUE) or not (FALSE)
+#' @param not_cumu Logical, whether to return the df before the cumulative sum is taken (TRUE) or return the final cumulated sum (FALSE)
+#' @return Data frame with a new column: either discounted (not_cumu) or cumulative discounted over
+#' the whole period (by_decade = FALSE) or by decade (by_decade = TRUE)
+calc_cumulated_discounted_sum_costsonly <- function(lockin_mif_em, start_date, end_date, discount_rate, col_to_sum, final_col_name, by_decade = FALSE, not_cumu = FALSE) {
+    lockin_mif_em <- lockin_mif_em %>%
+        group_by(region, category) %>%
+        complete(period = full_seq(period, 1)) %>%  # Fill every year
+        arrange(region,category, period) %>%
+        # in million USD, or in MtCO2
+        mutate(value = !!sym(col_to_sum)) %>%
+        fill(value, .direction = "down") %>%
+        ungroup() %>%
+        select(-all_of(col_to_sum)) %>%
+        # correct time period. 2025 = 2023-2028 in REMIND
+        mutate(period = period - 2) %>% 
+        filter(between(period, start_date, end_date)) %>%
+        arrange(region, category, period) %>%
+        mutate(discounted_value = value / ((1 + discount_rate) ^ (period - start_date))) %>%
+        mutate(decade = get_decade(period))
+    if (by_decade) {
+        lockin_mif_em <- lockin_mif_em %>%
+            group_by(region, category, decade) %>%
+            summarise(!!final_col_name := sum(discounted_value, na.rm = TRUE)) %>%
+            ungroup()
+        return(lockin_mif_em)
+        }
+    # option to return the df before the cumulative sum is taken
+    if (not_cumu) {
+        return(lockin_mif_em)
+    }
+    lockin_mif_em <- lockin_mif_em %>%
+        group_by(region, category) %>%
+        summarise(!!final_col_name := sum(discounted_value, na.rm = TRUE)) %>%
+        ungroup()
+}
+
 # --- Main analysis and plotting function ---
 #' Run the abatement analysis to extract abatement costs between two scenarios
 #' and plot the results
@@ -283,7 +332,6 @@ run_abatement_analysis <- function(scen1, scen2, region_to_aggr, start_date, end
     mif2_em <- get_total_emissions(mif2)
     costs1 <- calc_total_costs(lcop1_no_em_total, mif1_prod)
     costs2 <- calc_total_costs(lcop2_no_em_total, mif2_prod)
-
     # Demand adjustment
     # adjusts for the loss of welfare arising from less steel production
     # using delta_production * avg_steel_price
@@ -293,17 +341,16 @@ run_abatement_analysis <- function(scen1, scen2, region_to_aggr, start_date, end
     costs2_adj <- add_demand_adjustment(costs2, demand_value)
 
     # Discounted sums
-    costs1_disc <- calc_cumulated_discounted_sum(costs1, start_date, end_date, discount_rate, "total_cost", "cost1")
-    costs2_disc <- calc_cumulated_discounted_sum(costs2_adj, start_date, end_date, discount_rate, "total_cost", "cost2")
+    costs1_disc <- calc_cumulated_discounted_sum_costsonly(costs1, start_date, end_date, discount_rate, "total_cost", "cost1")
+    costs2_disc <- calc_cumulated_discounted_sum_costsonly(costs2_adj, start_date, end_date, discount_rate, "total_cost", "cost2")
     em1_total <- calc_cumulated_discounted_sum(mif1_em, start_date, end_date, discount_rate, "total_CO2_yearly", "em1")
     em2_total <- calc_cumulated_discounted_sum(mif2_em, start_date, end_date, discount_rate, "total_CO2_yearly", "em2")
     em1_total_nodiscount <- calc_cumulated_discounted_sum(mif1_em, start_date, end_date, 0, "total_CO2_yearly", "em1_nodiscount")
     em2_total_nodiscount <- calc_cumulated_discounted_sum(mif2_em, start_date, end_date, 0, "total_CO2_yearly", "em2_nodiscount")
 
-
     # Calculate abatement cost
     fscp_df <- costs1_disc %>%
-        left_join(costs2_disc, by = "region") %>%
+        left_join(costs2_disc, by = c("region", "category")) %>%
         left_join(em1_total, by = "region") %>%
         left_join(em2_total, by = "region") %>%
         left_join(em1_total_nodiscount, by = "region") %>%
@@ -315,6 +362,7 @@ run_abatement_analysis <- function(scen1, scen2, region_to_aggr, start_date, end
     grouped_df <- imap_dfr(region_to_aggr, function(regions, new_region) {
         fscp_df %>%
             filter(region %in% regions) %>%
+            group_by(category) %>%
             summarise(
                 region = new_region,
                 cost1 = sum(cost1, na.rm = TRUE),
@@ -333,48 +381,77 @@ run_abatement_analysis <- function(scen1, scen2, region_to_aggr, start_date, end
     fscp_df_grouped <- fscp_df %>%
         filter(!region %in% new_regions_flattened) %>%
         bind_rows(grouped_df)
-    
+
     fscp_df_grouped <- fscp_df_grouped %>%
         mutate(region = factor(region, levels = region_order)) %>%
         arrange(region)
 
     fscp_df_grouped <- fscp_df_grouped %>%
-        select(region, fscp, em_diff) %>%
-        # arrange(region) %>%
+        select(region, category, fscp, em_diff) %>%
         filter(em_diff > 0) %>%
         # some regions have negative average abatement costs when looking at very small
         # abatement volumes (less than 100 MtCO2)
         filter(!(fscp < 0 & em_diff < 200)) %>%
-        mutate(em_diff = em_diff / 1000) %>% # convert to GtCO2
-        # arrange(fscp) %>%
+        mutate(em_diff = em_diff / 1000) # convert to GtCO2
+    
+    total_fscp <- fscp_df_grouped %>%
+        group_by(region) %>%
+        summarise(total = sum(fscp)) %>%
+        ungroup()
+
+    fscp_df_grouped <- fscp_df_grouped %>%
+        left_join(total_fscp, by = "region") %>%
+        # calculate ymin (where the rectangles start)
+        mutate(category = factor(category, levels = c("non-energy","energy"))) %>%
+        group_by(region) %>%
+        arrange(region, category) %>%
+        mutate(ymin = lag(fscp, default = 0),
+               ymax = fscp+ lag(fscp, default = 0)) %>%
+        ungroup() %>%
+        arrange(total) %>%
+        group_by(category) %>%
         mutate(cum_em = cumsum(em_diff),
                xmin = cum_em - em_diff,
             #    region = factor(region, levels = region),
                xmax = cum_em)
+        
+    region_order_by_total <- total_fscp %>% arrange(total) %>% pull(region)
+
+    region_display_names <- c(
+      "EUR+NEU+USA+JPN+REF+CAZ" = "Global North",
+      "CHA" = "China",
+      "IND" = "India",
+      "OASLAM" = "Other Asia & Latin America",
+      "MEA" = "Middle East & Africa",
+      "SSA" = "Sub-Saharan Africa"
+    )
+
+    fscp_df_grouped$region <- factor(fscp_df_grouped$region, levels = region_order_by_total)
+    print(fscp_df_grouped$region)
 
     # Plotting
     plot_abatement_cost <- function(df, discount_rate, start_date, end_date, scen1_name, scen2_name) {
-        ggplot(df, aes(xmin = xmin, xmax = xmax, ymin = 0, ymax = fscp, fill = region)) +
+        ggplot(df, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = region, alpha = category)) +
             geom_rect(color = "black") +
             labs(
                 title = paste("Average abatement costs:", scen1_name, "to", scen2_name, "\n(discount rate:", discount_rate * 100, "%, dates:", start_date, "-", end_date, ")"),
                 x = "Cumulative emission difference (GtCo2)",
                 y = "Average abatement cost (USD/tCO2)"
             ) +
-            scale_fill_manual(values = c(
+            scale_fill_manual(
+                name = "Region",
+                values = c(
                 "CHA" = "#dc050c",
                 "IND" = "#f1932d",
-                "EUR" = "#1965b0",
                 "EUR+NEU+USA+JPN+REF+CAZ" = "#1965b0",
-                "OAS" = "#f7f056",
                 "OASLAM" = "#f7f056",
-                "NEU" = "#1ea6f0",
-                "REF" = "#b2df8a",
-                "USA" = "#33a02c", 
-                "JPN" = "#f8f2f2",
-                "LAM" = "#a65628",
                 "SSA" = "#4eb265",
-                "MEA" = "#ae76a3")) +
+                "MEA" = "#ae76a3"),
+                labels = region_display_names) +
+            scale_alpha_manual(values = c("energy" = 0.6, "non-energy" = 1),
+                    name = "Cost",
+                    labels = c("energy" = "Energy", "non-energy" = "Non-energy"),
+                    guide = guide_legend(reverse = TRUE)) +
             ylim(0, 350) +
             xlim(0,70)
     }
@@ -437,14 +514,30 @@ run_scenario_cost_andCO2value_comparison <- function(scen1, scen2, region_to_agg
     demand_value <- calc_demand_adjustment(mif1, mif2, mif2_prod, mif1_prod)
     costs2_adj <- add_demand_adjustment(costs2, demand_value)
 
+    # we do not need the different categories (energy and non-energy cost) here. Sum over them:
+    costs1 <- costs1 %>%
+        group_by(period, region) %>%
+        summarise(total_cost = sum(total_cost, na.rm = TRUE)) %>%
+        ungroup()
+    costs2_adj <- costs2_adj %>%
+        group_by(period, region) %>%
+        summarise(total_cost = sum(total_cost, na.rm = TRUE)) %>%
+        ungroup()
+
     # calculate co2 price in the least ambitious scenario (scen 1)
     # and multiply by emission difference to get the value of the avoided
     # co2 emissions. MtCO2/yr * USD/tCO2 = million USD/yr
     # (this corresponds to the cost of abating those additional emissions in scenario 1,
     # if they have not been avoided, they are abated at the marginal cost = co2 price of scenario 1)
     mif1_co2price <- get_co2_price(mif1)
+    mif2_co2price <- get_co2_price(mif2)
+    avg_co2price <- mif1_co2price %>%
+        left_join(mif2_co2price, by = c("region", "period")) %>%
+        mutate(co2_price = (co2_price.x + co2_price.y) / 2) %>%
+        select(period, region, co2_price)
+
     em_diff <- calc_em_difference(mif1_em, mif2_em)
-    value_co2_avoided <- mif1_co2price %>%
+    value_co2_avoided <- avg_co2price %>%
         left_join(em_diff, by = c("region", "period")) %>%
         mutate(value_em = co2_price * em_diff) %>%
         select(period, region, value_em)
